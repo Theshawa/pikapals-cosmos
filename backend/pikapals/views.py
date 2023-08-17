3ca595205e2dbe3bd2ff81bfb87e4186c133eded
lakshith <lakshith.k.nishshanke@gmail.com>
2023-08-17 05:49:27 +0530

    Implement CRUD for port model
    csrf exempt is temporarily used for postman testing

--- backend/pikapals/views.py
+++ backend/pikapals/views.py
@@ -1,23 +1,22 @@
 import json
 
 from django.core.exceptions import BadRequest
-from django.http import HttpResponse, HttpRequest
-
-from backend.pikapals.models import Port
+from django.http import HttpResponse, HttpRequest, HttpResponseNotFound
+from django.views.decorators.csrf import csrf_exempt
+from pikapals.models import Port
 
 
 def index(_):
     return HttpResponse("Hello, world")
 
 
+@csrf_exempt
 def port_endpoint(request: HttpRequest):
     response: HttpResponse = HttpResponse("")
     if request.method == "GET":
         response: HttpResponse = _port_get(request)
     elif request.method == "POST":
         response: HttpResponse = _port_post(request)
-    elif request.method == "UPDATE":
-        response: HttpResponse = _port_update(request)
     elif request.method == "DELETE":
         response: HttpResponse = _port_delete(request)
 
@@ -37,12 +36,25 @@ def _port_post(request: HttpRequest) -> HttpResponse:
 
 
 def _port_get(request: HttpRequest) -> HttpResponse:
-    pass
+    get_data = request.GET
+    if "id" not in get_data.keys():
+        # return all ports
+        ports = Port.objects.all()
+        data = {"is_success": True, "data": [port.to_data() for port in ports]}
+        return HttpResponse(json.dumps(data))
 
 
-def _port_update(request: HttpRequest) -> HttpResponse:
-    pass
+    data = {"is_success": True, "data": port.to_data()}
+    return HttpResponse(json.dumps(data))
 
 
 def _port_delete(request: HttpRequest) -> HttpResponse:
-    pass
+    delete_data = request.POST
+    if "id" not in delete_data.keys():
+        raise BadRequest("id is required")
+
+    port = Port.objects.get(id=delete_data["id"])
+    port.delete()
+
+    data = {"is_success": True}
+    return HttpResponse(json.dumps(data))
